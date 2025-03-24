e9e21acf9473ca70c94482d195cb36c4dab37a11
Bapi0204 <bapi.tulshihata@gmail.com>
2025-03-24 07:59:57 +0530

    Initial commit

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ itineraries = {
 }
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
 
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
